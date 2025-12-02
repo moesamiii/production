e3cf85ae49d038bd6a83c4302a7e623720e97bac
moesamiii <mohammedsamiii900@gmail.com>
2025-12-02 15:31:10 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,4 @@
-// Supabase Configuration
-const SUPABASE_URL = "https://kfsutnshxukcebymoitu.supabase.co";
-const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtmc3V0bnNoeHVrY2VieW1vaXR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ1OTAxMjcsImV4cCI6MjA4MDE2NjEyN30.0Hk5IaAHlCSNWkrEGOjYdSViAX5zMYb25IOBYdc9El4";
-
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+import { supabase } from "./config.js";
 
 // User Session
 let currentUser = {
